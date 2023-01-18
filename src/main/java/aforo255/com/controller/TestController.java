1b9207ddd9306410e30bce55bc7d498ccddda932
Stywar <61365253+Stywar@users.noreply.github.com>
2023-01-17 21:04:30 -0500

    Update TestController.java

--- src/main/java/aforo255/com/controller/TestController.java
+++ src/main/java/aforo255/com/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
 	@GetMapping("/test")
 	public String TestFirstJob() {
-		return "Welcome to MasterDevops |Tomcat v2345 | Ansible , AFORO255 09112022";
+		return "Welcome to MasterDevops |Tomcat v2.0.0 | Ansible , AFORO255 17012023";
 	}
 }
